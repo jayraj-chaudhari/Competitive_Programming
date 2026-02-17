079b7c6ec888860085f500a208ddc18b19dc8332
jayraj-chaudhari <u25ai046@aid.svnit.ac.in>
2026-02-17 13:41:34 +0530

    easy

---
+++ 800/2191A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        int flag=0;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if((arr[i]%2 ==1 && arr[i+1]%2==1) || (arr[i]%2 ==0 && arr[i+1]%2==0))
+            {
+                cout<<"NO\n";
+                flag=1;
+                break;
+            }
+        } 
+        if(flag==0)
+        {
+            cout<<"YES\n";
+        }
+    }
+    return 0;
+}
